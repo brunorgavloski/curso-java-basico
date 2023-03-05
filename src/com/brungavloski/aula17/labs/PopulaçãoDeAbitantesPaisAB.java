4d8177e432b3bfa4e83c39e68ad5e8d08661ec1b
Bruno Gavloski <bruno.gavloski@gmail.com>
2023-03-05 00:19:38 -0300

    lab 17 ex 5

--- src/com/brungavloski/aula17/labs/PopulaçãoDeAbitantesPaisAB.java
+++ src/com/brungavloski/aula17/labs/PopulaçãoDeAbitantesPaisAB.java
@@ -1,16 +1,83 @@
 package com.brungavloski.aula17.labs;
 
+import java.util.Scanner;
+
 public class PopulaçãoDeAbitantesPaisAB {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int popA = 80000;
-		int popB = 200000;
+		Scanner scan = new Scanner(System.in);
+		
+		boolean validar = false;
+		double popA;
+		double popB;
+		double taxaA;
+		double taxaB;
+		
+		
+		do {
+			
+			System.out.println("Entre opulação A; ");
+			popA = scan.nextInt();
+			
+			if (popA > 0) {
+				validar = true;
+			} else {
+				System.out.println("Pop precisa ser maior que zero.");
+			}
+			
+		} while (!validar);
+		
+			validar = false;
+		
+		do {
+			
+			System.out.println("Entre opulação B; ");
+			popB = scan.nextInt();
+			
+			if (popB > 0) {
+				validar = true;
+			} else {
+				System.out.println("Pop B precisa ser maior que zero.");
+			}
+			
+		} while (!validar);
+		
+		validar = false;
+		
+		do {
+			
+			System.out.println("Entre taxa A; ");
+			taxaA = scan.nextDouble();
+			
+			if (taxaA > 0) {
+				validar = true;
+			} else {
+				System.out.println("taxa B precisa ser maior que zero.");
+			}
+			
+		} while (!validar);
+		
+		validar = false;
+		
+		do {
+			
+			System.out.println("Entre taxa B; ");
+			taxaB = scan.nextDouble();
+			
+			if (taxaB > 0) {
+				validar = true;
+			} else {
+				System.out.println("taxa B precisa ser maior que zero.");
+			}
+			
+		} while (!validar);
+		
 		int cont = 0;
 		
+		
 		while (popA < popB) {
-			popA += (popA/100) * 3;
-			popB += (popA/100) * 1.5;
+			popA += (popA/100) * taxaA;
+			popB += (popA/100) * taxaB;
 			cont++;
 		}
 		
