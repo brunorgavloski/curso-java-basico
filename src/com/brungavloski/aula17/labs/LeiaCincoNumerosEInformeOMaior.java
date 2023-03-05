18b61adbfd5c64f4acaf9d91246d6cd8d11f747e
Bruno Gavloski <bruno.gavloski@gmail.com>
2023-03-05 20:49:23 -0300

    aula17 exer7 refatoração

--- src/com/brungavloski/aula17/labs/LeiaCincoNumerosEInformeOMaior.java
+++ src/com/brungavloski/aula17/labs/LeiaCincoNumerosEInformeOMaior.java
@@ -7,6 +7,9 @@ public class LeiaCincoNumerosEInformeOMaior {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		
+		/*
+		 
 		int count = 0;
 		double numberOne = 0;
 		double numberTwo = 0;
@@ -45,6 +48,21 @@ public class LeiaCincoNumerosEInformeOMaior {
 			}
 			maior = true;
 		} while(!maior);
+		*/
+		
+		int num;
+		int maior = Integer.MIN_VALUE;
+		
+		for (int i = 0; i <5 ; i++) {
+			System.out.println("Entre com um número: ");
+			num = scan.nextInt();
+			
+			if (num > maior) {
+				maior = num;
+			}
+		}
+		
+		System.out.println("O maior número digitado foi: " + maior);
 		
 	}
 }
