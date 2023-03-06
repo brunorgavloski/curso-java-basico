2658b5f98681d95f2e090a7a2df4c8bab1090966
Bruno Gavloski <bruno.gavloski@gmail.com>
2023-03-05 21:04:01 -0300

    aula17 exer9

---
+++ src/com/brungavloski/aula17/labs/ImprimaNumerosImpares.java
@@ -0,0 +1,16 @@
+package com.brungavloski.aula17.labs;
+
+public class ImprimaNumerosImpares {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int  i = 1; i < 50 ;i++) {
+			
+			if ( i % 2 != 0) {
+				System.out.println("O numero impar é: " + i);
+			}
+		}
+	}
+
+}
