4d4987185df7289798a3ce21a13d873e85b75e42
Bruno Gavloski <bruno.gavloski@gmail.com>
2023-03-06 19:32:14 -0300

    lab17 exerc. 10

---
+++ src/com/brungavloski/aula17/labs/RecebeDoisInteirosGereOIntervalo.java
@@ -0,0 +1,29 @@
+package com.brungavloski.aula17.labs;
+
+import java.util.Scanner;
+
+public class RecebeDoisInteirosGereOIntervalo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int um = 0;
+		int dois = 0;
+		System.out.println("Digite o número inteiro 1: ");
+		um = scan.nextInt();
+		
+		System.out.println("Digite o número inteiro 2: ");
+		dois = scan.nextInt();
+		
+		if (um >= dois) {
+			for (int i = dois; i<um ; i++) {
+				System.out.println("O numero do meio é: " + i);
+			}
+			
+		} else if (dois >= um) {
+			for (int i = um; i<dois ; i++) {
+				System.out.println("O numero do meio é: " + i);
+			}
+		}
+	}
+
+}
