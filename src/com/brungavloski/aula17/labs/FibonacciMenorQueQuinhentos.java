742aa476ade45387e1ce6c4f0c6d1f5ade0c5b1a
Bruno Gavloski <bruno.gavloski@gmail.com>
2023-03-06 21:01:32 -0300

    lab17 exerc. 16

---
+++ src/com/brungavloski/aula17/labs/FibonacciMenorQueQuinhentos.java
@@ -0,0 +1,25 @@
+package com.brungavloski.aula17.labs;
+
+public class FibonacciMenorQueQuinhentos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean teste = false;
+		int primeiro = 1;
+		int segundo =1;
+		int proximo = 0;
+		do {
+			
+			proximo = primeiro + segundo;
+			primeiro = segundo;
+			segundo = proximo;
+			
+			
+			if (proximo > 500)
+				teste =true;
+			System.out.println(segundo);
+		} while (!teste);
+	}
+
+}
