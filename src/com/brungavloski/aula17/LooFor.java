36b388c6401bb851359c2bea13ef63281d371809
Bruno Gavloski <gavloski11@gmail.com>
2023-03-11 20:46:05 -0300

    aula18 break e continue

--- src/com/brungavloski/aula17/LooFor.java
+++ src/com/brungavloski/aula17/LooFor.java
@@ -4,10 +4,16 @@ public class LooFor {
 
 	public static void main(String[] args) {
 		
-		for (int i = 0; i < 5 ; i++) {
+		for (int i = 0; i <= 10 ; i++) {
 			System.out.println("i tem valor: " + i);
+		
+			double resultado = 5*i+3;
+			
+			System.out.println("resultado:" + resultado);
 		}
 		
+		
+		/**
 		for (int i=5; i > 0; i--) {
 			System.out.println("i tem valor: " + i);
 		}
@@ -15,6 +21,8 @@ public class LooFor {
 		for (int i=0, j=10 ; i < j ; i++, j--) {
 			System.out.println("i = " + i + "; j = " + j);
 		}
+		
+		*/
 
 	}
 
