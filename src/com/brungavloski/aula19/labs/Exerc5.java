c7c0b2923318b59ec82c8b4fba961ebc2e8b4ce4
Bruno Gavloski <gavloski11@gmail.com>
2023-03-14 20:05:14 -0300

    exerc5

--- src/com/brungavloski/aula19/labs/Exerc5.java
+++ src/com/brungavloski/aula19/labs/Exerc5.java
@@ -1,32 +1,28 @@
 package com.brungavloski.aula19.labs;
 
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class Exerc4 {
+public class Exerc5 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int[] vetorA = new int[15];
-		double[] vetorB = new double[vetorA.length];
+		int[] vetorA = new int[10];
+		int[] vetorB = new int[vetorA.length];
 		
 		for (int i = 0; i < vetorA.length; i++) {
-			
-			System.out.println("Digite o valor do indice " + i + ": ");
+			System.out.println("Digite o valor do para o índice " + i + " :");
 			vetorA[i] = scan.nextInt();
 			
-			vetorB[i] = Math.sqrt(vetorA[i]);
+			vetorB[i] = vetorA[i] * i;
 			
 		}
-
+		
 		System.out.println();
 		System.out.print("VetorA: ");
 		for (int i = 0; i < vetorA.length; i++ ) {
 			System.out.print(vetorA[i] + " ");
 		}
-		
-	//	DecimalFormat df = new DecimalFormat("###,###.###");
 		
 		System.out.println();
 		System.out.print("VetorB: ");
