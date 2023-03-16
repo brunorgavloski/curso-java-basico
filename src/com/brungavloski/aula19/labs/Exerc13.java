297fff6a0be41dfdd483339f5c7a01de33d010f1
Bruno Gavloski <gavloski11@gmail.com>
2023-03-15 21:14:08 -0300

    exerc13

--- src/com/brungavloski/aula19/labs/Exerc13.java
+++ src/com/brungavloski/aula19/labs/Exerc13.java
@@ -2,19 +2,21 @@ package com.brungavloski.aula19.labs;
 
 import java.util.Scanner;
 
-public class Exerc12 {
+public class Exerc13 {
 
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		
-		int[] vetorA = new int[10];
 		int caunt = 0;
+		int[] vetorA = new int[10];
+		
 		for (int i = 0; i < vetorA.length; i++) {
-			System.out.println("Entre com o valor do indice " +i+ ": ");
+			System.out.println("Digite o valor para o indice " + i + " :");
 			vetorA[i] = scan.nextInt();
-			caunt = caunt + vetorA[i];
 			
+			if (vetorA[i] % 5 == 0) {
+				caunt += vetorA[i];
+			}
 		}
 		
 		System.out.print("VetorA: ");
@@ -23,7 +25,7 @@ public class Exerc12 {
 		}
 		
 		System.out.println("A soma do vetor é: " + caunt);
-
+		
 	}
 
 }
